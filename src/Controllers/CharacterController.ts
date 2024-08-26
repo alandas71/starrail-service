8dcfbc4bef36f1ababd5846803a647c48d3746de
alandas71 <alanalves.dev@gmail.com>
2024-08-26 09:19:18 -0300

    Feat: agora retorna os stats corretamente

--- src/Controllers/CharacterController.ts
+++ src/Controllers/CharacterController.ts
@@ -37,33 +37,42 @@ class CharacterController {
   }
   
   async getAllCharacteres(req: Request, res: Response): Promise<void> {
-    const { page, limit, query } = req.query;
+    const { page, limit, query, level, ascension } = req.query;
     const currentPage = parseInt(page as string, 10) || 1;
     const itemsPerPage = parseInt(limit as string, 10) || 20;
     const startIndex = (currentPage - 1) * itemsPerPage;
 
     try {
-      let characters = await this.starRailService.getAllCharacteres();
-  
-      if (query) {
-        const searchTerm = (query as string).toLowerCase();
-        characters = characters.filter(c => 
-          c.name.get("en").toLowerCase().includes(searchTerm)
-        );
-      }
+        let characters = await this.starRailService.getAllCharacteres();
+    
+        if (query) {
+          const searchTerm = (query as string).toLowerCase();
+          characters = characters.filter(c => 
+            c.name.get("en").toLowerCase().includes(searchTerm)
+          );
+        }
 
-      const paginatedCharacters = characters.slice(startIndex, startIndex + itemsPerPage);
-      const safeCharacters = paginatedCharacters.map((c: any) => {
+        const paginatedCharacters = characters.slice(startIndex, startIndex + itemsPerPage);
+        const safeCharacters = paginatedCharacters.map((c: any) => {
+        
+        const stats = c.getStatsByLevel(ascension ? parseInt(ascension as string) : 0, level ? parseInt(level as string) : 1);
+            
+        const statsObject = stats.reduce((acc: any, stat: any) => {
+            acc[stat.type] = stat.value;
+            return acc;
+        }, {});
+
         return _.cloneDeep({
           id: c.id,
-          charIcon: c.icon.url,
           stars: c.stars,
-          pathName: c.path.id,
-          pathIcon: c.path.icon.url,
-          combatType: c.combatType.id,
           maxEnergy: c.maxEnergy,
+          charIcon: c.icon.url,
+          pathIcon: c.path.icon.url,
           combatTypeIcon: c.combatType.icon.url,
-          nameEnglish: c.name.get("en")
+          combatType: c.combatType.name.get("en"),
+          pathName: c.path.name.get("en"),
+          nameEnglish: c.name.get("en"),
+          stats: statsObject
         });
       });
   
@@ -75,6 +84,7 @@ class CharacterController {
 
   async getCharacterById(req: Request, res: Response): Promise<void> {
     const { id } = req.params;
+    const { level, ascension } = req.query;
 
     try {
       const characters = await this.starRailService.getAllCharacteres();
@@ -85,16 +95,24 @@ class CharacterController {
         return;
       }
 
+      const stats = character.getStatsByLevel(ascension ? parseInt(ascension as string) : 0, level ? parseInt(level as string) : 1);
+            
+      const statsObject = stats.reduce((acc: any, stat: any) => {
+          acc[stat.type] = stat.value;
+          return acc;
+      }, {});
+
       const safeCharacter = _.cloneDeep({
         id: character.id,
-        icon: character.icon.url,
         stars: character.stars,
-        pathName: character.path.id,
-        pathIcon: character.path.icon.url,
-        combatType: character.combatType.id,
         maxEnergy: character.maxEnergy,
+        charIcon: character.icon.url,
+        pathIcon: character.path.icon.url,
         combatTypeIcon: character.combatType.icon.url,
-        name: character.name.get("en"),
+        combatType: character.combatType.name.get("en"),
+        pathName: character.path.name.get("en"),
+        nameEnglish: character.name.get("en"),
+        stats: statsObject,
       });
 
       res.status(200).json(safeCharacter);
